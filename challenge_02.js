608e00de1130dc788ba6a9ce7cc430a7229bdbd5
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-10 12:54:15 +0100

    Getränke-Challenge | Beispiel 1

--- challenge_02.js
+++ challenge_02.js
@@ -21,14 +21,32 @@ Alter 13-17 // Cola
 /***************************************************************/
 /************   Getränke-Challenge | Beispiel 1    *************/
 /***************************************************************/
-let firstName = prompt( "bitte gib Deinen Namen ein: " );
-let personAge = parseInt( prompt( "Bitt gib Dein Alter ein:" ) )
+const checkAge = true;
 
-switch (personAge) {
-    case value:
-        
+// let firstName = prompt( "bitte gib Deinen Namen ein: " );
+// let personAge = parseInt( prompt( "Bitt gib Dein Alter ein:" ) );
+let firstName = "Sandro";
+let personAge = 3;
+
+let isBaby = ( personAge >= 0 && personAge <= 5 ) ? true : false;
+let isKid = ( personAge >= 0 && personAge <= 5 ) ? true : false;
+let isTeen = ( personAge >= 0 && personAge <= 5 ) ? true : false;
+let isAdault = ( personAge > 18 ) ? true : false;
+
+switch (checkAge) {
+    case isBaby:
+        console.log( firstName + ", Du darfst nur Milch trinken!" );
         break;
-
+    case isKid:
+        console.log( firstName + ", Du darfst Saft trinken!" );
+        break;
+    case isTeen:
+        console.log( firstName + ", Du darfst schon Cola trinken!" );
+        break;
+    case isAdault:
+        console.log( firstName + ", Du darfst auch Wein trinken!" );
+        break;
     default:
+        console.log( firstName + ", Du hast kein richtiges Alter eingegeben und darfst daher gar nichts trinken!" );
         break;
 }
